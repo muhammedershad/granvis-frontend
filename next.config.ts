63ee464d64a6fd6fa3430daeaf3fa2e00e68fafd
Ershad-personal <muhammedershadp@gmail.com>
2026-03-04 02:53:31 +0530

    build error fix

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,6 @@ const cloudfrontHostname = cloudfrontDomain
   : null;
 
 const nextConfig: NextConfig = {
-  // Use separate output dir for builds so it doesn't clobber the dev server's .next
-  ...(process.env.BUILD_DIR ? { distDir: process.env.BUILD_DIR } : {}),
   images: {
     remotePatterns: [
       {
